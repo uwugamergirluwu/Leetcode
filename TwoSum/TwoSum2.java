25656f0ee18f83fddbaed5d6611180f6e69307ad
Keith <113943336+uwugamergirluwu@users.noreply.github.com>
2025-09-16 11:49:50 -0400

    Create TwoSum2.java

---
+++ TwoSum/TwoSum2.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            int diff = target - nums[i];
+            if (numMap.containsKey(diff)){
+                return new int[]{numMap.get(diff), i};
+            }
+            numMap.put(nums[i], i);
+        }
+    return null;
+    }
+}
