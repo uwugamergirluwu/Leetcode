998c4a24383ca7b7a154791b221cdeb2a68fda1c
Keith <113943336+uwugamergirluwu@users.noreply.github.com>
2025-09-24 16:05:51 -0400

    Create Solution2.java

---
+++ 9 - PalindromeNumber/Solution2.java
@@ -0,0 +1,15 @@
+class Solution {
+    //Solution without the use of typecasting
+    public boolean isPalindrome(int x) {
+        if (x >= 0 && x < 10) return true;
+        if (x < 0 || x % 10 == 0 && x != 0){
+            return false;
+        }
+        int reversedHalf = 0;
+        while (x > reversedHalf){
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+        return (x == reversedHalf || x == reversedHalf / 10);
+    }
+}
