191036b4e52bbf47b62941e18c562fd477cf2783
Keith <113943336+uwugamergirluwu@users.noreply.github.com>
2025-10-07 10:37:40 -0400

    Update Solution.java

--- 5 - LongestPalindromicSubstr/Solution.java
+++ 5 - LongestPalindromicSubstr/Solution.java
@@ -1,5 +1,24 @@
 class Solution {
     public String longestPalindrome(String s) {
-        
+        String longest = "";
+        for (int i = 0; i < s.length(); i++){
+            String odd = returnPalindrome(s, i, i);
+            String even = returnPalindrome(s, i, i+1);
+            
+            String longer = odd.length() > even.length() ? odd : even;
+            if (longer.length() > longest.length()){
+                longest = longer;
+            }
+        }
+        return longest;
+    
+    }
+
+    private String returnPalindrome(String s, int left, int right){
+        while (left >=0 && right < s.length() && s.charAt(left)==s.charAt(right)){
+            left--;
+            right++;
+        }
+        return (s.substring(left + 1, right));
     }
 }
