afee638addb8a96f44be51990f2496dcbb4a4a85
Keith <keithtran02@gmail.com>
2025-11-16 18:09:23 -0500

    Python solution to 104

---
+++ 104 MaxDepthofBinaryTree/Solution.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        if not root:
+            return 0
+        height_left = self.maxDepth(root.left)
+        height_right = self.maxDepth(root.right)
+
+        return (max(height_left, height_right) + 1)
